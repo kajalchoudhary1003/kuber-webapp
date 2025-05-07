11a049b9b32b6e0ffce58ccbdb7f1884706f0b0d
Dhruv Rawat <dhruvrawatt9@gmail.com>
2025-05-07 20:49:32 +0530

    backour route fixed

--- backend/routes/backupRoutes.js
+++ backend/routes/backupRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const backupController = require('../controllers/backupController');
 
-router.post('/backup', backupController.backupDatabase);
+router.post('/', backupController.backupDatabase);
 router.post('/restore', backupController.restoreDatabase);
 
 module.exports = router;
