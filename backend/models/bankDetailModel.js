eaa2cabbec05d7df20e7a2bc3c85ef2759c7f010
Dhruv Rawat <dhruv.rawat@corevaluetech.com>
2025-05-07 06:58:40 +0530

    random

--- backend/models/bankDetailModel.js
+++ backend/models/bankDetailModel.js
@@ -20,5 +20,5 @@ const bankDetailSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 const BankDetail = mongoose.model('BankDetail', bankDetailSchema);
-
+// gh
 module.exports = BankDetail;
